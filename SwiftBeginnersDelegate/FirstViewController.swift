b1ebcc0f1a5bb9e68a3a129af7af6cbf008f9fee
中山 陽介 <ihatenonoildressing@gmail.com>
2017-07-01 09:03:08 +0900

    Initial Commit

---
+++ SwiftBeginnersDelegate/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftBeginnersDelegate
+//
+//  Created by 中山 陽介 on 2017/07/01.
+//  Copyright © 2017年 Yosuke Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
